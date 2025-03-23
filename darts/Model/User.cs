a9e094f69b58ca93647f8a023aa6f0f77e0e3579
mmalesinski <misialek1998@gmail.com>
2025-03-23 22:13:42 +0100

    newGamePage in progress

---
+++ darts/Model/User.cs
@@ -0,0 +1,7 @@
+namespace darts.Model;
+
+public class User
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+}
--- darts/ViewModel/NewGameViewModel.cs
+++ darts/ViewModel/NewGameViewModel.cs
@@ -1,24 +1,73 @@
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using darts.Model;
 
 namespace darts.ViewModel;
 
-public class NewGameViewModel : INotifyPropertyChanged
+public class NewGameViewModel : BaseViewModel
 {
+    public ICommand StartNewGameCommand { get; set; }
+    public ICommand AddNewUserCommand { get; set; }
+    public ICommand RemoveUserCommand { get; set; }
+    public bool CanStartNewGame { get; set; }
+    public List<GameMode> Games { get; set; }
+
+    private string _newUserName;
+
+    public string NewUserName
+    {
+        get =>  _newUserName;
+        set => SetProperty(ref _newUserName, value);
+    }
+    public ObservableCollection<User> Users { get; set; }
     public NewGameViewModel()
     {
-        StartNewGameCommand = new Command(async void () =>
-        {
-            await Shell.Current.GoToAsync(nameof(GamePage));
-        });
+        Games = GameModes.Modes;
+        Users = [];
+        StartNewGameCommand = new Command(StartNewGame);
+        AddNewUserCommand = new Command(AddNewUser);
+        RemoveUserCommand = new Command<Guid>(RemoveUser);
     }
 
-    public event PropertyChangedEventHandler? PropertyChanged;
-    public Command StartNewGameCommand { get; set; }
+    private async void StartNewGame()
+        => await Shell.Current.GoToAsync(nameof(GamePage));
 
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    private async void AddNewUser()
     {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        var currentPage = Application.Current?.MainPage;
+        if (Users.Select(x => x.Name).Contains(NewUserName))
+        {
+            if (currentPage is not null)
+            {
+                var toast = Toast.Make("Taki użytkownik już istnieje", ToastDuration.Long);
+                await toast.Show();
+            }
+            return;
+        }
+        Users.Add(new User{
+            Id = Guid.NewGuid(),
+            Name = NewUserName
+        });
+        CanStartNewGame = true;
+        NewUserName = string.Empty;
     }
-    
+
+    private async void RemoveUser(Guid userId)
+    {
+        var currentPage = Application.Current?.MainPage;
+        var userToRemove = Users.SingleOrDefault(x => x.Id == userId);
+        if (userToRemove is not null && currentPage is not null)
+        {
+            var confirmation = await currentPage.DisplayAlert(
+                "Potwierdzenie", 
+                "Czy na pewno chcesz usunąć tego użytkownika?", 
+                "Tak", 
+                "Nie");
+            if(confirmation)
+                Users.Remove(userToRemove);
+        }
+    }
+
 }
